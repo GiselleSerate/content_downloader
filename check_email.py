1d29be5a5bc33d55df31e1ea83984e93f309b53f
GiselleSerate <giselle.serate@gmail.com>
2019-06-13 15:20:41 -0700

    Write assorted email checking code

--- check_email.py
+++ check_email.py
@@ -1,10 +1,20 @@
 from guerrillamail import GuerrillaMailSession
+from bs4 import BeautifulSoup
+
 
 session = GuerrillaMailSession(email_address='phoenix@guerrillamailblock.com')
 print(session.get_session_state()['email_address'])
 email_list = session.get_email_list()
 for email_summary in email_list:
     email = session.get_email(email_summary.guid)
-    print(email.body)
+    mailsoup = BeautifulSoup(email.body, 'html5lib')
+    try:
+        header = mailsoup.find('h1')
+        otpElement = header.find_next_sibling('p').find_next_sibling('p')
+        otpCode = otpElement.string.strip()
+        print(otpCode)
+    except Exception as e:
+        print(e)
+        print('Didn\'t get OTP code') # TODO handle errors better. 
 
 # hqt7en+3x1o341y10qc@sharklasers.com
